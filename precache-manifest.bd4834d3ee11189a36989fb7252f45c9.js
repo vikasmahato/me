4c24de127d8bb77c4b8922a3bbf5528cbcf7ad2d
Vikas Mahato <vikasmahato0@gmail.com>
2019-10-19 23:26:17 +0530

    Updates

---
+++ precache-manifest.bd4834d3ee11189a36989fb7252f45c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34146a7acf96c76af0521917da5481ca",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "f11e9618646e067624cb",
+    "url": "/me/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "d0b9a492c4ca7382e508",
+    "url": "/me/static/js/2.1201c609.chunk.js"
+  },
+  {
+    "revision": "f11e9618646e067624cb",
+    "url": "/me/static/js/main.5a5ae9a5.chunk.js"
+  },
+  {
+    "revision": "bb9b31d2761e3fd4b6fe",
+    "url": "/me/static/js/runtime-main.d049d15d.js"
+  }
+]);
