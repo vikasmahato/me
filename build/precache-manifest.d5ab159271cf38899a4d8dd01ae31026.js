f357a01ca288117646acd29f3b2483427e039e51
Vikas Mahato <vikasmahato0@gmail.com>
2019-10-20 11:18:48 +0530

    Fixed typos

---
+++ build/precache-manifest.d5ab159271cf38899a4d8dd01ae31026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0422038dde4dcbf36593123c5e1cf5",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "05a3c8d3cf9561eb9e05",
+    "url": "/me/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "d0b9a492c4ca7382e508",
+    "url": "/me/static/js/2.1201c609.chunk.js"
+  },
+  {
+    "revision": "05a3c8d3cf9561eb9e05",
+    "url": "/me/static/js/main.b95eb587.chunk.js"
+  },
+  {
+    "revision": "bb9b31d2761e3fd4b6fe",
+    "url": "/me/static/js/runtime-main.d049d15d.js"
+  }
+]);
